d0ddd49150cd08bfddd524febc3ad20b9070c728
cl-davii <davifsc03@gmail.com>
2025-02-11 15:14:25 -0300

    Exercício extra de fixação do conteúdo

--- section09/extra/bankAcc/Account.java
+++ section09/extra/bankAcc/Account.java
@@ -1,4 +1,43 @@
 package section09.extra.bankAcc;
 
 public class Account {
+
+    private static int number;
+    private String name;
+    private double balance;
+
+    public Account(int number, String name, double balance) {
+        Account.number = number;
+        this.name = name;
+        this.balance = balance;
+    }
+
+    public static int getNumber() {
+        return number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void deposit(double value) {
+        balance += value;
+    }
+
+    public void withdraw(double value) {
+        balance -= (value + 5);
+    }
+
+    @Override
+    public String toString() {
+        return "Account\n" + "Number: " + number + " - Holder: " + name + " - Balance: " + balance;
+    }
 }
--- section09/extra/bankAcc/Main.java
+++ section09/extra/bankAcc/Main.java
@@ -1,4 +1,48 @@
 package section09.extra.bankAcc;
 
+import java.util.Locale;
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        Random random = new Random();
+        Account account;
+
+        System.out.print("The account number is: ");
+        int accountNumber = random.nextInt(1, 100);
+        System.out.println(accountNumber);
+
+        System.out.print("Type the name of account holder: ");
+        String holder = scanner.nextLine();
+
+        System.out.print("Do you want to do a initial deposit in your account? [y/n]: ");
+        char initialDepositOption = scanner.next().charAt(0);
+
+        if (initialDepositOption == 'y') {
+            System.out.print("Enter the value to the initial deposit: ");
+            double balance = scanner.nextDouble();
+            account = new Account(accountNumber, holder, balance);
+        } else {
+            double balance = 0d;
+            account = new Account(accountNumber, holder, balance);
+        }
+
+        System.out.println(account);
+
+        System.out.print("Enter the quantity to deposit: ");
+        double depositValue = scanner.nextDouble();
+        account.deposit(depositValue);
+
+        System.out.println(account);
+
+        System.out.print("Enter the quantity to withdraw: ");
+        double wdValue = scanner.nextDouble();
+        account.withdraw(wdValue);
+
+        System.out.println(account);
+
+        scanner.close();
+    }
 }
