fdb082f9d6d186e5f800c9daaebe1340fda9a8f7
cl-davii <davifsc03@gmail.com>
2025-02-11 19:51:20 -0300

    Exercício de fixação sobre vetores

--- section10/extra/boardingHouse/Client.java
+++ section10/extra/boardingHouse/Client.java
@@ -1,4 +1,33 @@
 package section10.extra.boardingHouse;
 
 public class Client {
+
+    private String name;
+    private String mail;
+
+    public Client(String name, String mail) {
+        this.name = name;
+        this.mail = mail;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    @Override
+    public String toString() {
+        return "Name -> " + name + " | Email -> " + mail;
+    }
 }
--- section10/extra/boardingHouse/Main.java
+++ section10/extra/boardingHouse/Main.java
@@ -1,4 +1,38 @@
 package section10.extra.boardingHouse;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        Client client;
+
+        Client[] bedrooms = new Client[10];
+
+        System.out.print("Enter the number of rooms to be rented[1-10]: ");
+        int n = scanner.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            System.out.println("Rent #" + (i + 1));
+            System.out.print("Name: ");
+            String name = scanner.next();
+
+            System.out.print("E-mail: ");
+            String mail = scanner.next();
+
+            System.out.print("Choose an bedroom[0-9]: ");
+            int bedroom = scanner.nextByte();
+            client = new Client(name, mail);
+
+            bedrooms[bedroom] = client;
+        }
+        System.out.println("Occupied rooms");
+        for (int i = 0; i < bedrooms.length; i++) {
+            if (bedrooms[i] != null) {
+                System.out.println(i + ": " + bedrooms[i]);
+            }
+        }
+        scanner.close();
+    }
 }
