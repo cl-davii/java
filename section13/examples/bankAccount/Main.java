5e739debd7a1af06348164710d093860b1ae4aca
cl-davii <davifsc03@gmail.com>
2025-02-17 09:08:06 -0300

    Exercício resolvido sobre abstração

---
+++ section13/examples/bankAccount/Main.java
@@ -0,0 +1,12 @@
+package section13.examples.bankAccount;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+
+    }
+}
